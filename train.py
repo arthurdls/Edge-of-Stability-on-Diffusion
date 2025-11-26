b7b0c3dbd46dd23156c29570de017be3984b2e91
Arthur De Los Santos <arthurdls04@gmail.com>
2025-11-26 17:14:15 -0500

    change to MNIST from CIFAR10

--- train.py
+++ train.py
@@ -30,15 +30,15 @@ def seed_everything(seed=42):
 
 seed_everything()
 
-# CIFAR-10 data loaders
 def get_dataloaders(batch_size=128, img_size=32, num_workers=4):
     transform = transforms.Compose([
         transforms.Resize(img_size),
         transforms.ToTensor(),
-        transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)),  # map to [-1,1]
+        # transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)), # map to [-1, 1] for CIFAR10
+        transforms.Normalize((0.5,), (0.5,)),  # map to [-1,1] for MNIST
     ])
-    train_ds = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    test_ds = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    train_ds = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    test_ds = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
     train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
     test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
     return train_loader, test_loader
@@ -66,12 +66,12 @@ LR_dict = {
     1e-3: '_lr_1e-3',
     1e-4: '_lr_1e-4',
     1e-5: '_lr_1e-5',
-    # 5e-4: '_lr_5e-4',
-    # 5e-5: '_lr_5e-5',
+    5e-4: '_lr_5e-4',
+    5e-5: '_lr_5e-5',
 }
 
 TIMESTEPS = 1000
-EPOCHS = 100
+EPOCHS = 200
 
 print("--- RUNNING IMPLEMENTATIONS ---")
 for LR, TEST_NAME_ADD_ON in LR_dict.items():
@@ -82,9 +82,9 @@ for LR, TEST_NAME_ADD_ON in LR_dict.items():
     print(f"Timesteps: {TIMESTEPS}")
 
     unet_setting = {
-        'in_ch':3,
-        'base_ch':64,
-        'time_emb_dim':64
+        'in_ch':1, # 1 for MNIST, 3 for CIFAR10
+        'base_ch':32, # 32 for MNIST, 64 for CIFAR10
+        'time_emb_dim':32, # 32 for MNIST, 64 for CIFAR10
     }
     schedule_settings = {
         'timesteps':TIMESTEPS,
